c75769d0051c84073ed3ba448541c59003c25cba
Temniy Daiver <orcanlend@narod.ru>
2023-10-18 20:07:15 +0300

    modified:   App.js 	modified:   package-lock.json modified:   package.json

--- App.js
+++ App.js
@@ -4,11 +4,33 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>tHIS IS GAME TIC TAC TOE!!!</Text>
+      
+      <View style={{flexDirection:"row"}}>
+        <View style={[styles.tile, {borderLeftWidth: 0, borderTopWidth:0}]} />
+        <View style={[styles.tile, {borderTopWidth: 0}]} />
+        <View style={[styles.tile, {borderTopWidth:0, borderRightWidth:0}]} />
+      </View>
+
+      <View style={{flexDirection:"row"}}>
+        <View style={[styles.tile, {borderLeftWidth:0}]} />
+        <View style={[styles.tile]} />
+        <View style={[styles.tile,{borderRightWidth:0}]} />
+      </View>
+
+      <View style={{flexDirection:"row"}}>
+        <View style={[styles.tile, {borderLeftWidth:0, borderBottomWidth:0}]} />
+        <View style={[styles.tile, {borderBottomWidth:0}]} />
+        <View style={[styles.tile, {borderRightWidth:0, borderBottomWidth:0}]} />
+      </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +39,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tile:{
+    borderWidth: 15,
+    width:100,
+    height:100,
+  },
+  tileX:{
+    color: 'red',
+  }
 });
