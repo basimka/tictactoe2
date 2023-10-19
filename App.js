d6fc354cd1ad47648809dae61e87f20b9027a911
Temniy Daiver <orcanlend@narod.ru>
2023-10-19 14:49:11 +0300

    modified:   App.js

--- App.js
+++ App.js
@@ -38,34 +38,37 @@ export default class App extends React.Component {
     const NUM_TILES = 3;
     var arr = this.state.gameState;
     var sum;
+    
 
-
-    //chek rows
-    for (var i = 0 ; 1 < NUM_TILES; i++){
-      sum = arr[i][0] + arr [i][1] + arr[i][2];
-      if (sum == 3) {return 1;}
-      else if (sum == -3) {return -1}
-    }
-
-    //chek columns
-    for (i=0; 1< NUM_TILES; i++){
-      sum = arr[0][i] + arr [1][i] + arr [2][i];
-      if (sum == 3) {return 1;}
-      else if (sum == -3) {return -1}
-
-    }
-
-    //check the diagonals
-    sum = arr[0][0] + arr [1][1] + arr [2][2];
+   //chek rows
+   for (var i = 0; 1 < NUM_TILES; i++){
+    sum = arr[i][0] + arr [i][1] + arr[i][2];
     if (sum == 3) {return 1;}
     else if (sum == -3) {return -1}
-
-    sum = arr[2][0] + arr [1][1] + arr [0][2];
+  }
+  //chek columns
+   for (var i = 0; 1 < NUM_TILES; i++){
+    sum = arr[0][i] + arr [1][i] + arr[2][i];
     if (sum == 3) {return 1;}
     else if (sum == -3) {return -1}
+  }
 
-    //the are no winners
-    return 0;
+ 
+
+   //check the diagonals
+   sum = arr[0][0] + arr [1][1] + arr [2][2];
+   if (sum == 3) {return 1;}
+   else if (sum == -3) {return -1}
+
+   sum = arr[2][0] + arr [1][1] + arr [0][2];
+   if (sum == 3) {return 1;}
+   else if (sum == -3) {return -1}
+
+   //the are no winners
+   return 0;
+
+    
+   
 
   }
 
